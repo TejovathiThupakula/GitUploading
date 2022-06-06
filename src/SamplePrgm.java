a2526dc1392c19af14889b05f7a7871746796ba3
Tejovathi <tejut.naidu@gmail.com>
2022-06-06 23:27:23 +0530

    committeds to uplaod

--- src/SamplePrgm.java
+++ src/SamplePrgm.java
@@ -1,6 +1,6 @@
 public class SamplePrgm {
     public static void main(String[] args) {
         System.out.println("to uplaod into git");
-        System.out.println("to uplaoded into git");
+        System.out.println("to uplaodedd into git");
     }
 }
